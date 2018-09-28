ebc9f012178ecf4b0b9928c752f438edbe339223
Jeff Diederiks <jeff.diederiks@gmail.com>
2018-09-28 01:49:23 -0400

    Continuing to rewrite scripts. Made progress on the createNewSlave.sh script

--- jenkins/scp/createNewSlave.sh
+++ jenkins/scp/createNewSlave.sh
@@ -1,4 +1,32 @@
 #! /bin/bash
 
-sudo touch /home/ec2-user/output.txt
-echo "CREATE NEW SLAVE, MAN!" >> /home/ec2-user/output.txt
+# Pull project name from a safe location
+#PROJECT_NAME=$(sed -e 's/-jenkins//g' <<< $(cat /home/ec2-user/.ssh/authorized_keys | grep -Po '[[:space:]][a-zA-Z]*\-jenkins$'))
+
+# Configure the ECS CLI to use the correct cluster and region
+#ecs-cli configure --cluster $PROJECT_NAME-jenkins-slaves --region us-east-2 --default-launch-type EC2 --config-name $PROJECT_NAME-jenkins-slaves
+
+# Query how many slaves are currently in the cluster
+#CURRENT_SLAVE_COUNT=$(aws ecs describe-clusters --cluster $PROJECT_NAME-jenkins-slaves --region us-east-2 | jq '.clusters[0].registeredContainerInstancesCount')
+
+# Scale up the cluster
+#ecs-cli scale --cluster $PROJECT_NAME-jenkins-slaves --size $(($CURRENT_SLAVE_COUNT + 1)) --region us-east-2 --capability-iam
+
+# Set the Jenkins password variable
+JENKINS_PASSWORD=$(/home/ec2-user/printJenkinsPassword.sh)
+
+# Get a Jenkins "crumb" for authentication
+JENKINS_CRUMB=$(curl -s "http://admin:"$JENKINS_PASSWORD"@localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)")
+
+# Get all unique node numbers used in Jenkins
+NODE_NUMBERS=()
+NODES=$(jq -r '.computer' <<< $(curl -H $JENKINS_CRUMB -L -s -u admin:$JENKINS_PASSWORD localhost:8080/computer/api/json))
+NODE_COUNT=$(echo $NODES | jq '. | length')
+for (( NODE_INDEX=0; NODE_INDEX<NODE_COUNT; NODE_INDEX++ )) do
+  NODE_NAME=$(sed -e 's/^"//g' -e 's/"$//g' <<< $(echo $NODES | jq '.['"$NODE_INDEX"'].displayName'))
+  NODE_OFFLINE=$(sed -e 's/^"//g' -e 's/"$//g' <<< $(echo $NODES | jq '.['"$NODE_INDEX"'].offline'))
+  if [ $NODE_NAME != "master" ] && [ $NODE_OFFLINE == "false" ]; then
+    NODE_NUMBERS+=($(sed -e 's/slave//g' <<< $NODE_NAME))
+  fi
+done
+echo ${NODE_NUMBERS[*]}
--- jenkins/scp/provisionSlaves.sh
+++ jenkins/scp/provisionSlaves.sh
@@ -4,20 +4,44 @@
 # Depending on the size of the build queue, this script can kick off the provisioning of a new Jenkins slave
 # TODO: Lop off the top of .build-queue-size if it is more than 360 lines (two hours worth of records)
 
-JENKINS_PASSWORD=\$(/home/ec2-user/printJenkinsPassword.sh)
-JENKINS_CRUMB=\$(curl -s "http://admin:"\$JENKINS_PASSWORD"@localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)")
-echo \$(curl -H \$JENKINS_CRUMB -L -s -u admin:\$JENKINS_PASSWORD http://localhost:8080/queue/api/json | jq ".items | length") >> /home/ec2-user/slaves/.build-queue-size
+# Get a Jenkins "crumb" for authentication
+function fetchJenkinsCrumb() {
+  JENKINS_CRUMB=$(curl -s "http://admin:"$JENKINS_PASSWORD"@localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)")
+}
 
-BUILD_QUEUE_MEASURE_ONE=\$(tail -n 3 /home/ec2-user/slaves/.build-queue-size | head -n 1)
-BUILD_QUEUE_MEASURE_TWO=\$(tail -n 2 /home/ec2-user/slaves/.build-queue-size | head -n 1)
-BUILD_QUEUE_MEASURE_THREE=\$(tail -n 1 /home/ec2-user/slaves/.build-queue-size)
-if [[ \$BUILD_QUEUE_MEASURE_ONE -gt 0 ]] && [[ \$BUILD_QUEUE_MEASURE_TWO -gt 0 ]] && [[ \$BUILD_QUEUE_MEASURE_THREE -gt 0 ]]; then
-  if [ ! -f /home/ec2-user/slaves/.slave-creation-lock ]; then
-    touch /home/ec2-user/slaves/.slave-creation-lock
-    /home/ec2-user/slaves/createNewSlave.sh
-  fi 
-else
-  if [ -f /home/ec2-user/slaves/.slave-creation-lock ]; then
-    rm /home/ec2-user/slaves/.slave-creation-lock
+# Look to the `.build-queue-size` for the two most recent measurements
+function getTwoMostRecentBuildQueueMeasurements() {
+  BUILD_QUEUE_MEASURE_ONE=$(tail -n 2 /home/ec2-user/slaves/.build-queue-size | head -n 1)
+  BUILD_QUEUE_MEASURE_TWO=$(tail -n 1 /home/ec2-user/slaves/.build-queue-size)
+}
+
+# Run the `createNewSlave.sh` script if the build queue has extra work
+# Note: The `.slave-creation-lock` file prevents this function from launching a new slave if one is already being launched
+function kickoffSlaveCreationIfBuildQueueHasWork() {
+  if [[ $BUILD_QUEUE_MEASURE_ONE -gt 0 ]] && [[ $BUILD_QUEUE_MEASURE_TWO -gt 0 ]]; then
+    if [ ! -f /home/ec2-user/slaves/.slave-creation-lock ]; then
+      touch /home/ec2-user/slaves/.slave-creation-lock
+      /home/ec2-user/slaves/createNewSlave.sh
+    fi 
+  else
+    if [ -f /home/ec2-user/slaves/.slave-creation-lock ]; then
+      rm /home/ec2-user/slaves/.slave-creation-lock
+    fi
   fi
-fi
+}
+
+# Record the current build queue size in the `.build-queue-size` log
+function recordCurrentBuildQueueSize() {
+  echo $(curl -H $JENKINS_CRUMB -L -s -u admin:$JENKINS_PASSWORD http://localhost:8080/queue/api/json | jq ".items | length") >> /home/ec2-user/slaves/.build-queue-size
+}
+
+# Set the Jenkins password variable
+function setJenkinsPassword() {
+  JENKINS_PASSWORD=$(/home/ec2-user/printJenkinsPassword.sh)
+}
+
+setJenkinsPassword
+fetchJenkinsCrumb
+recordCurrentBuildQueueSize
+getTwoMostRecentBuildQueueMeasurements
+kickoffSlaveCreationIfBuildQueueHasWork
